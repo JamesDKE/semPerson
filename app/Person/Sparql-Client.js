8e233a74b38b4a2201cda190ece6759c8af2b9cf
Daniel <daniel.kaltenbrunner@gmail.com>
2015-05-14 18:36:08 +0200

    Sparql-Client

---
+++ app/Person/Sparql-Client.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Daniel on 14/05/15.
+ */
+
+
+var SparqlClient = require('sparql-client');
+var util = require('util');
+var endpoint = 'http://dbpedia.org/sparql';
+
+// Get the leaderName(s) of the given citys
+// if you do not bind any city, it returns 10 random leaderNames
+var query = 'SELECT * FROM <http://dbpedia.org> WHERE { ?city <http://dbpedia.org/property/leaderName> ?leaderName} LIMIT 10';
+var client = new SparqlClient(endpoint);
+console.log("Query to " + endpoint);
+console.log("Query: " + query);
+client.query(query)
+    //.bind('city', 'db:Chicago')
+    //.bind('city', 'db:Tokyo')
+    //.bind('city', 'db:Casablanca')
+    //.bind('city', 'db:Vienna')
+    .execute(function(error, results) {
+        process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
+    });
+
+// rdfs:label =
