43c7cead625a9e3712d6fd86e26a2006c617d702
Daniel <daniel.kaltenbrunner@gmail.com>
2015-05-15 12:59:16 +0200

    Sparql-Client

--- app/Person/Sparql-Client.js
+++ app/Person/Sparql-Client.js
@@ -1,25 +1,84 @@
 /**
  * Created by Daniel on 14/05/15.
  */
-
-
 var SparqlClient = require('sparql-client');
 var util = require('util');
 var endpoint = 'http://dbpedia.org/sparql';
 
-// Get the leaderName(s) of the given citys
-// if you do not bind any city, it returns 10 random leaderNames
-var query = 'SELECT * FROM <http://dbpedia.org> WHERE { ?city <http://dbpedia.org/property/leaderName> ?leaderName} LIMIT 10';
-var client = new SparqlClient(endpoint);
-console.log("Query to " + endpoint);
-console.log("Query: " + query);
-client.query(query)
-    //.bind('city', 'db:Chicago')
-    //.bind('city', 'db:Tokyo')
-    //.bind('city', 'db:Casablanca')
-    //.bind('city', 'db:Vienna')
-    .execute(function(error, results) {
-        process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
-    });
 
-// rdfs:label =
+function getLocation(company){
+    var companyRecource = "<http://dbpedia.org/resource/"+company+">";
+
+    var locations = [];
+
+    var query = "SELECT * WHERE { "+companyRecource+" <http://dbpedia.org/property/locationCity> ?locationCity. } LIMIT 10";
+    var client = new SparqlClient(endpoint);
+    console.log("Query to " + endpoint);
+    console.log("Query: " + query);
+    client.query(query)
+
+        .execute(function(error, results) {
+            var result = (JSON.stringify(arguments, null, 20, true));
+
+            var parsed = JSON.parse(result);
+            parsed = parsed[1].results.bindings;
+
+
+
+            for(var x in parsed){
+                locations.push(parsed[x].locationCity.value);
+            }
+
+            var index;
+
+            for (index = 0; index < locations.length; ++index) {
+                console.log(locations[index]);
+            }
+        });
+
+        return locations;
+}
+
+function getLocationCoordinate(locationURI){
+
+   var coordinates = [];
+
+   var query = "SELECT * WHERE { "+locationURI+" geo:lat ?lat } LIMIT 10";
+   var client = new SparqlClient(endpoint);
+    console.log("Query to " + endpoint);
+    console.log("Query: " + query);
+    client.query(query)
+
+        .execute(function(error, results) {
+            var result = (JSON.stringify(arguments, null, 20, true));
+
+            var parsed = JSON.parse(result);
+            coordinates[0] = parsed[1].results.bindings[0].lat.value;
+
+            console.log(coordinates[0]);
+
+            //console.log(result);
+        });
+
+    var query = "SELECT * WHERE { "+locationURI+" geo:long ?long } LIMIT 10";
+    var client = new SparqlClient(endpoint);
+    console.log("Query to " + endpoint);
+    console.log("Query: " + query);
+    client.query(query)
+
+        .execute(function(error, results) {
+            var result = (JSON.stringify(arguments, null, 20, true));
+
+            var parsed = JSON.parse(result);
+            coordinates[1] = parsed[1].results.bindings[0].long.value;
+
+            console.log(coordinates[1]);
+
+            //console.log(result);
+        });
+
+    return coordinates;
+}
+
+getLocationCoordinate("<http://dbpedia.org/resource/Armonk,_New_York"+">");
+//getLocation("IBM");
